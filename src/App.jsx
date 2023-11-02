e4acaf64fc893c7dc77210ae858ee15a7386a0bf
Laucf7 <laura.casanovas.freixa@gmail.com>
2023-11-02 19:51:30 +0100

    Exercici-1

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Card from "./Card"
+import { useState } from 'react';
+
+
+export default function App() {
+
+  const [step,setStep] = useState(0); 
+  
+  const tutorialData = [
+    {
+      title: "Dedica moltes hores",
+      description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
+      bgColor: "#fff",
+      image: "",
+    },
+    {
+      title: "Programa projectes propis",
+      description: "Més val 10 hores treballant en projectes propis que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
+      bgColor: "#fff",
+      image: "",
+    },
+    {
+      title: "Procura descansar",
+      description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
+      bgColor: "#fff",
+      image: "",
+    }
+    ];
+
+  return (
+    <>
+      <Card {...tutorialData[step]}/>
+    </>
+  )
+}
