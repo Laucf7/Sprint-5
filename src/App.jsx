be4b5720193aa861aadc2ba979e2f64b054341e9
Laucf7 <laura.casanovas.freixa@gmail.com>
2023-11-03 23:57:20 +0100

    Exercici-3

--- src/App.jsx
+++ src/App.jsx
@@ -17,27 +17,29 @@ export default function App() {
     {
       title: "Dedica moltes hores",
       description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
-      bgColor: "#fff",
-      image: "",
+      bgColor: "#2a9d8f",
+      image: "./src/assets/time_managment.svg",
     },
     {
       title: "Programa projectes propis",
       description: "Més val 10 hores treballant en projectes propis que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
-      bgColor: "#fff",
-      image: "",
+      bgColor: "#adb5bd",
+      image: "./src/assets/programming.svg",
     },
     {
       title: "Procura descansar",
       description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-      bgColor: "#fff",
-      image: "",
+      bgColor: "#ffc300",
+      image: "./src/assets/meditation.svg",
     }
     ];
 
   return (
     <>
       <Card 
-        title= {tutorialData[step].title} 
+        bgColor = {tutorialData[step].bgColor}
+        image = {tutorialData[step].image}
+        title = {tutorialData[step].title} 
         description={tutorialData[step].description}
         clickNext={nextStep}
         />
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 
-export default function Card(props){
-    
+
+export default function Card(props) {
+    const bgColorImage = {
+        backgroundColor: props.bgColor,
+        height: "65%",
+        overflow: "hidden",
+        borderTopLeftRadius: "1rem",
+        borderTopRightRadius: "1rem",
+        display: "flex", 
+        alignItems: "center", 
+        justifyContent: "center",
+    };
+
+    const cardStyle = {
+        width: "22rem",
+        height: "40rem",
+        border: "none",
+        borderRadius: "1rem",
+        boxShadow: "0 5px 7px",
+    }
+
+    const buttonStyle = {
+        width: "3rem",
+        height: "3rem",
+        float: "right",
+        borderRadius: "50%",
+        backgroundColor: "black",
+        color: "white",
+    }
+
+
     return (
-     <>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        <button onClick={props.clickNext}> NEXT </button>
-     </>
+        <div className="card-full" style={cardStyle} >
+            <div className="image-container" style={bgColorImage} >
+                <img src={props.image} style={{ width: "100%"}}/>
+            </div>
+            <div className="text-card" style={{ padding: "1rem"}}>
+                <h2>{props.title}</h2>
+                <p>{props.description}</p>  
+                <button onClick={props.clickNext} style={buttonStyle}> {"→"} </button>
+            </div>
+        </div>
+
     );
 }
